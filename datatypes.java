d090ba7245e8523b7753e93240862af90adec9a5
Davin <71451687+Davin-oss@users.noreply.github.com>
2020-09-24 11:28:51 -0400

    Update datatypes.java

--- datatypes.java
+++ datatypes.java
@@ -1,38 +1,32 @@
-import java.util.Scanner;
-public class datatypes {
+mport java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
-	public static void main(String[] args) 
-	{
-		  /* Declare second integer, double, and String variables. */
-        int i;
-        double d;
-        String s;
-
-        /* Read and save an integer, double, and String to your variables.*/
-        Scanner scanner= new Scanner(System.in);
-        
-        i=scanner.nextInt();
-        d=scanner.nextDouble();
-        s=scanner.next();
-        
-        System.out.println(i);
-        System.out.println(d);
-        System.out.println(s);
-        
-        // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
-        
-        /* Print the sum of both integer variables on a new line. */
-
-        /* Print the sum of the double variables on a new line. */
+public class Solution {
+	
+    public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
 		
-        /* Concatenate and print the String variables on a new line; 
-
-       the 's' variable above should be printed first. */
+        Scanner scan = new Scanner(System.in);
+        /* Declare second integer, double, and String variables. */
+      int j=scan.nextInt();
+    /* Read and save an integer, double, and String to your variables.*/
+    double k=scan.nextDouble();
+    scan.nextLine();
+    String str=scan.nextLine();
+    /* Print the sum of both integer variables on a new line. */
+    System.out.println(i+j);
+    /* Print the sum of the double variables on a new line. */
+    System.out.println(d+k);
+    /* Concatenate and print the String variables on a new line;
+    the 's' variable above should be printed first. */
+ 
+    System.out.println(s+ "" +str);
+        
      
-	
-        scanner.close();
-	}
 
-	
-	
-}
+        scan.close();
